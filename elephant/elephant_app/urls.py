dd83ae7a422210c6c3ccff9e7f1f1dc79311b29c
Artur <waligoraartur95@gmail.com>
2025-02-02 12:47:06 +0100

    new category/new product forms

--- elephant/elephant_app/urls.py
+++ elephant/elephant_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.hello_world, name='hello_world'),
+    path("add_category", views.add_category, name="add_category"),
+    path('add_item', views.add_item, name='add_item'),
 ]
 
